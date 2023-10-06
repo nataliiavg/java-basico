857d5065d68855be190a5703133c426b373fa424
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-06 18:48:38 -0300

    Add running through the Win terminal

---
+++ src/aula/terminal_e_argumentos/MinhaClasse.java
@@ -0,0 +1,8 @@
+package aula.terminal_e_argumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Executado com sucesso pelo terminal!");
+    }
+    
+}
