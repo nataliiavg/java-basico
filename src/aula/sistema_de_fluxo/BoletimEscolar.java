c4a03347e2a34cebcfcd7c372952502f58fa1a2d
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-07 15:11:33 -0300

    Add example of compound conditional structure

---
+++ src/aula/sistema_de_fluxo/BoletimEscolar.java
@@ -0,0 +1,12 @@
+package aula.sistema_de_fluxo;
+
+public class BoletimEscolar {
+    public static void main(String[] args) {
+        double nota = 6.5 ;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado");
+    }
+}
