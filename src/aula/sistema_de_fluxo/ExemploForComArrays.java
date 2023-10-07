846dbc64ef36fef5623eba16e930d192608dc42d
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-07 16:11:39 -0300

    Add For repetition structure in Arrays

---
+++ src/aula/sistema_de_fluxo/ExemploForComArrays.java
@@ -0,0 +1,12 @@
+package aula.sistema_de_fluxo;
+
+public class ExemploForComArrays {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        }
+
+    }
+}
