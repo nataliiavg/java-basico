51d9eb1ab0324f5001982987dab3ea9cb95feaf7
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-07 17:11:29 -0300

    Add do while repetition structure example

--- src/aula/sistema_de_fluxo/ExemploDoWhile.java
+++ src/aula/sistema_de_fluxo/ExemploDoWhile.java
@@ -4,21 +4,28 @@ import java.util.Random;
 
 public class ExemploDoWhile {
     public static void main(String[] args) {
-        System.out.println("Discando...");
+        // System.out.println("Discando...");
 
-        do {
-            // executando repetidas vezes até alguém atender
-            System.out.println("Telefone tocando");
+        // do {
+        // // executando repetidas vezes até alguém atender
+        // System.out.println("Telefone tocando");
 
-        } while (tocando());
+        // } while (tocando());
 
-        System.out.println("Alô? ");
-    }
+        // System.out.println("Alô? ");
+        // }
 
-    private static boolean tocando() {
-        boolean atendeu = new Random().nextInt(3) == 1;
-        System.out.println("Atendeu? " + atendeu);
-        // negando o ato de continuar tocando
-        return !atendeu;
+        // private static boolean tocando() {
+        // boolean atendeu = new Random().nextInt(3) == 1;
+        // System.out.println("Atendeu? " + atendeu);
+        // // negando o ato de continuar tocando
+        // return !atendeu;
+        // }
+
+        int numero = 1;
+        for (int x=1; x<2; x++){
+            numero = numero + x;
+        }
+        System.out.println("O valor de número é: " + numero);
     }
 }
